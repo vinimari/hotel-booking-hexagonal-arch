62037be452ef54ec2e8b25150ba2093307f5e2d1
vinimari <viniciusmarinho44@hotmail.com>
2025-09-24 22:35:47 -0300

    chore: changed group and artifact names

---
+++ service-registry/src/test/java/hotel_booking/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package hotel_booking.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
